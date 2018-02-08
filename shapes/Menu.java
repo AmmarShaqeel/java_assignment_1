50d855dffbfcdc5c8abd79efa0efc62bce2160a9
ammar903@gmail.com <ammar903@gmail.com>
2018-02-08 16:07:49 +0000

    done?

--- shapes/Menu.java
+++ shapes/Menu.java
@@ -1,9 +1,8 @@
  import java.util.*;
 
 public class Menu {
-    public int Menu(int i)
-        throws IllegalArgumentException, ArithmeticException {
-
+    public static void main(String[] args) {
+        
             int xval;
             int yval;
             double h;
@@ -11,104 +10,102 @@ public class Menu {
 
             Shape shapes[] = new Shape[100];
 
-            Scanner input = new Scanner(System.in);
-
-            System.out.print("Please input the ting you want: ");
-            System.out.println("2.Point 3.Circle 4.Cylinder 5.Print everyting");
-        
-            int choice = input.nextInt();
-
-            switch(choice) {
-                case 2: 
-                    System.out.println("input xval");
-                    xval = input.nextInt();
-
-                    System.out.println("input yval");
-                    yval = input.nextInt();
-
-                    Point point = new Point(xval, yval);
-                    shapes[i] = point;
-                    System.out.println(shapes[i].getName());
-                    break;
-
-                case 3: 
-                    System.out.println("input xval");
-                    xval = input.nextInt();
-
-                    System.out.println("input yval");
-                    yval = input.nextInt();
-
-                    System.out.println("input radius");
-                    r = input.nextDouble();
-
-                    Circle circle = new Circle(xval, yval, r);
-                    shapes[i] = circle;
-                    break;
-
-                case 4: 
-                    System.out.println("input xval");
-                    xval = input.nextInt();
-
-                    System.out.println("input yval");
-                    yval = input.nextInt();
-
-                    System.out.println("input radius");
-                    r = input.nextDouble();
-
-                    System.out.println("input height");
-                    h = input.nextDouble();
-
-                    Cylinder cylinder = new Cylinder(xval, yval, r, h);
-                    shapes[i] = cylinder;
-                    break;
- 
-                case 5: 
-                    System.out.println("Printing everything");
-
-                    for(int k=0;k<shapes.length;k++)
-                    {
-                         if(shapes[k] == null)
-                        {
-                            System.out.println("at the end");
-                            break;
-                        }
-
-                        System.out.println(shapes[k].getName());
-                        
-                   break;
-                    }
-
-                case 0: 
-                    System.out.println("exiting system");
-                    return 0;
-                            
-            }
-            return 1;
-    }
-
-    public static void main(String[] args) {
-
-      int flag = 1;
-      int i = 0;
+            int flag = 1;
+            int i = 0;
 
           while (flag == 1) {
 
-            Menu menu = new Menu();
-
             try {
-              flag = menu.Menu(i);
-              i++;
-            }
+                Scanner input = new Scanner(System.in);
+                System.out.print("Please input the ting you want: ");
+                System.out.println("2.Point 3.Circle 4.Cylinder 5.Print everyting 0.exit");
+        
+                System.out.print("i = ");
+                System.out.println(i);
 
-            catch (ArithmeticException e) {
-              System.err.printf("Exception: %s \n", e);
-              System.err.printf("Numbers should be positive integers different than zero   \n");
-            }
+                int choice = input.nextInt();
+                switch(choice) {
+ 
+                    case 0: 
+                        System.out.println("exiting system");
+                        System.exit(0);
 
-            catch (InputMismatchException e) {
-              System.err.printf("Exception: %s \n", e);
-              System.err.printf("Inputs should be integers \n");
+                    case 2: 
+                        System.out.println("input xval");
+                        xval = input.nextInt();
+
+                        System.out.println("input yval");
+                        yval = input.nextInt();
+
+                        Point point = new Point(xval, yval);
+                        shapes[i] = point;
+                        System.out.println(shapes[i].getName());
+                        i++;
+                        break;
+
+                    case 3: 
+                        System.out.println("input xval");
+                        xval = input.nextInt();
+
+                        System.out.println("input yval");
+                        yval = input.nextInt();
+
+                        System.out.println("input radius");
+                        r = input.nextDouble();
+
+                        Circle circle = new Circle(xval, yval, r);
+                        shapes[i] = circle;
+                        i++;
+                        break;
+
+                    case 4: 
+                        System.out.println("input xval");
+                        xval = input.nextInt();
+
+                        System.out.println("input yval");
+                        yval = input.nextInt();
+
+                        System.out.println("input radius");
+                        r = input.nextDouble();
+
+                        System.out.println("input height");
+                        h = input.nextDouble();
+
+                        Cylinder cylinder = new Cylinder(xval, yval, r, h);
+                        shapes[i] = cylinder;
+                        i++;
+                        break;
+ 
+                    case 5: 
+                        System.out.println("Printing everything");
+
+                        for(int k=0;k<shapes.length;k++)
+                        {
+                             if(shapes[k] == null)
+                            {
+                                System.out.println("at the end");
+                                break;
+                            }
+
+                            System.out.println(shapes[k].getName());
+                            
+                        }
+                        break;
+
+                    default:    
+                        System.out.println("invalid input, try again");
+                        break;
+               }
             }
+                catch (ArithmeticException e) {
+                System.err.printf("Exception: %s \n", e);
+                System.err.printf("Numbers should be positive integers different than zero   \n");
+                }
+
+                catch (InputMismatchException e) {
+                System.err.printf("Exception: %s \n", e);
+                System.err.printf("Inputs should be integers \n");
+                }
 
         }
  
